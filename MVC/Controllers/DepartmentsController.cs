2b9466372ad0900145364311b7f172a5e22452eb
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2023-09-05 18:07:58 -0300

    Refactoring namespaces and adding department

---
+++ MVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers;
+
+public class DepartmentsController : Controller
+{
+    public IActionResult Index()
+    {
+        List<Department> departments = new()
+        {
+            new Department { Id = 1, Name = "IT" },
+            new Department { Id = 2, Name = "Marketing" }
+        };
+
+        return View(departments);
+    }
+}
--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -1,32 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MVC.Models;
+using MVC.Models.ViewModels;
 using System.Diagnostics;
 
-namespace MVC.Controllers
+namespace MVC.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
