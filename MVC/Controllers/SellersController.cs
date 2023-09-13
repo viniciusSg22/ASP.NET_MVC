295851f0e14b05ca0b61dc5e325269d6ec63102b
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2023-09-13 17:32:24 -0300

    Sellers view and controller

---
+++ MVC/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
